47a0271c3b460003b2eb4245711275e6ec72bab0
Terell <Terelld@gmail.com>
2022-06-01 00:52:31 -0500

    commit tree 2c63026fb5d5b811b89a048535bfefa63c6856d1

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// function findMatching(object) {
+//     let drivers = drivers.filter(drivers, arr) => {
+//         return drivers.toLowerCase === drivers;
+// });
+// // }
+function findMatching(drivers, string) {
+    let match = drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+    return match
+}
+
+
+
+function fuzzyMatch(drivers, string) {
+    let firstLetters = drivers.filter(driver => driver.slice(0, string.length) === string.slice(0, string.length))
+    return firstLetters
+}
+
+function matchName(drivers, name){
+    return drivers.filter(function(driver){
+      return driver.name === name
+    })
+  }
+
+// function fuzzyMatch(drivers, name){
+//     const fuzzyDrivers = drivers.filter(function(person){
+//         return person.slice(0) == name.slice(0);
+//     });
+//     return fuzzyDrivers;
+// }
